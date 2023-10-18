d77f0ea8221aad01ad92466f7f690e9deca477ee
Avigail-Siman-Tov <avigailsi123@gmail.com>
2023-10-18 15:23:21 +0300

    chanes

--- src/route/apiPlanTrip.js
+++ src/route/apiPlanTrip.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 const planTrip = require('../controller/planTripController');
+const recommender = require('../controller/recommenderController');
 
 router.get('/samesizekmeans', planTrip.sameSizeKMeans)
+router.get('/recommender',recommender.recommendeCollaborativeFilterin)
+
 module.exports = router;
