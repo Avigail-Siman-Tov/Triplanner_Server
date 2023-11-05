af5a384b7b2d5c8ce6fa677df77fdab89941d328
Avigail-Siman-Tov <avigailsi123@gmail.com>
2023-11-05 16:35:44 +0200

    changes

--- src/controller/recommenderController.js
+++ src/controller/recommenderController.js
@@ -3,84 +3,84 @@ var jsrecommender = require("js-recommender");
 const Place = require("../model/Place");
 const TravelerPlaces = require('../model/TravelerPlaces');
 var recommender = new jsrecommender.Recommender();
-   
+
 const recommendeCollaborativeFilterin = async (req, res) => {
   console.log("query", req.query)
-  
-  TravelerPlaces.find({travelerMail:req.query.travelerMail},{ travelerMail: 1, placeId: 1,travelerPlaceRating: 1,_id: 0 })
-  .then(resTravelerPlaces=>{
 
-    TravelerPlaces.find({tripDestination:req.query.tripDestination},{ travelerMail: 1, placeId: 1,travelerPlaceRating: 1,tripDestination:1, _id: 0 })
-      .then(resUsersPlacesInDes=>{
-        const placeInDes = new  Set()
-        for(const k in resUsersPlacesInDes){
-          placeInDes.add(resUsersPlacesInDes[k].placeId)
-        }
-        const places= new Set()
-        for(const i in resTravelerPlaces){
-          if(!placeInDes.has(resTravelerPlaces[i])){
-            places.add(resTravelerPlaces[i].placeId)
+  TravelerPlaces.find({ travelerMail: req.query.travelerMail }, { travelerMail: 1, placeId: 1, travelerPlaceRating: 1, _id: 0 })
+    .then(resTravelerPlaces => {
+
+      TravelerPlaces.find({ tripDestination: req.query.tripDestination }, { travelerMail: 1, placeId: 1, travelerPlaceRating: 1, tripDestination: 1, _id: 0 })
+        .then(resUsersPlacesInDes => {
+          const placeInDes = new Set()
+          for (const k in resUsersPlacesInDes) {
+            placeInDes.add(resUsersPlacesInDes[k].placeId)
           }
-        }
-        TravelerPlaces.find({placeId:{$in:Array.from(places)}},{ travelerMail: 1, placeId: 1,travelerPlaceRating: 1, _id: 0 })
-        .then(resUsersPlaces=>{
-        var table = new jsrecommender.Table();
-        // table.setCell('[place-id]', '[traveler mail]', [score]);
-        for(const j in resUsersPlaces){
-          table.setCell(resUsersPlaces[j].placeId , resUsersPlaces[j].travelerMail, resUsersPlaces[j].travelerPlaceRating);
-        }
-        for(const i in resUsersPlacesInDes){
-          table.setCell(resUsersPlacesInDes[i].placeId , resUsersPlacesInDes[i].travelerMail, resUsersPlacesInDes[i].travelerPlaceRating);
-        }
-        console.log(table)
-        
-        var model = recommender.fit(table);
-        console.log(model);
-        
-        predicted_table = recommender.transform(table);
-        
-        console.log(predicted_table);
-       var recommendPlace=[]
-        for (var i = 0; i < predicted_table.columnNames.length; ++i) {
-            var user = predicted_table.columnNames[i];
-            if(user== req.query.travelerMail){
-              console.log('For user: ' + user);
-              for (var j = 0; j < predicted_table.rowNames.length; ++j) {
-                  var movie = predicted_table.rowNames[j];
-                  if( Number.isNaN(Math.round(table.getCell(movie, user)))){
-                  console.log('Movie [' + movie + '] has actual rating of ' + Math.round(table.getCell(movie, user)));
-                  console.log('Movie [' + movie + '] is predicted to have rating ' + Math.round(predicted_table.getCell(movie, user)));
-                  var x= Math.round(predicted_table.getCell(movie, user))
-                  if(x>3){
-                    recommendPlace.push(movie)
-                  }
-                  }
+          const places = new Set()
+          for (const i in resTravelerPlaces) {
+            if (!placeInDes.has(resTravelerPlaces[i])) {
+              places.add(resTravelerPlaces[i].placeId)
+            }
+          }
+          TravelerPlaces.find({ placeId: { $in: Array.from(places) } }, { travelerMail: 1, placeId: 1, travelerPlaceRating: 1, _id: 0 })
+            .then(resUsersPlaces => {
+              var table = new jsrecommender.Table();
+              // table.setCell('[place-id]', '[traveler mail]', [score]);
+              for (const j in resUsersPlaces) {
+                table.setCell(resUsersPlaces[j].placeId, resUsersPlaces[j].travelerMail, resUsersPlaces[j].travelerPlaceRating);
               }
-          }
-        }
-        console.log("placeInDes \n"+Array.from(placeInDes))
-        Place.find({placeId:{$in:Array.from(recommendPlace)}})
-        .then(result=>{
-          res.send(result)
+              for (const i in resUsersPlacesInDes) {
+                table.setCell(resUsersPlacesInDes[i].placeId, resUsersPlacesInDes[i].travelerMail, resUsersPlacesInDes[i].travelerPlaceRating);
+              }
+              console.log(table)
+
+              var model = recommender.fit(table);
+              console.log(model);
+
+              predicted_table = recommender.transform(table);
+
+              console.log(predicted_table);
+              var recommendPlace = []
+              for (var i = 0; i < predicted_table.columnNames.length; ++i) {
+                var user = predicted_table.columnNames[i];
+                if (user == req.query.travelerMail) {
+                  console.log('For user: ' + user);
+                  for (var j = 0; j < predicted_table.rowNames.length; ++j) {
+                    var movie = predicted_table.rowNames[j];
+                    if (Number.isNaN(Math.round(table.getCell(movie, user)))) {
+                      console.log('Movie [' + movie + '] has actual rating of ' + Math.round(table.getCell(movie, user)));
+                      console.log('Movie [' + movie + '] is predicted to have rating ' + Math.round(predicted_table.getCell(movie, user)));
+                      var x = Math.round(predicted_table.getCell(movie, user))
+                      if (x > 3) {
+                        recommendPlace.push(movie)
+                      }
+                    }
+                  }
+                }
+              }
+              console.log("placeInDes \n" + Array.from(placeInDes))
+              Place.find({ placeId: { $in: Array.from(recommendPlace) } })
+                .then(result => {
+                  res.send(result)
+                })
+                .catch(err => {
+                  res.send('false')
+                })
+
+            })
+            .catch(err => {
+              res.send('false')
+            })
+
         })
-        .catch(err=>{
+        .catch(err => {
           res.send('false')
         })
 
-      })
-      .catch(err=>{
-        res.send('false')
-      })
-     
     })
-    .catch(err=>{
+    .catch(err => {
       res.send('false')
     })
-   
-  })
-  .catch(err=>{
-    res.send('false')
-})
 }
 
-module.exports ={recommendeCollaborativeFilterin}
+module.exports = { recommendeCollaborativeFilterin }
